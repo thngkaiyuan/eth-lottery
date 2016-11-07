ce99bf960bd6ac4c1c049376b63e4dbf523e84c9
Xinan Liu <xinan@me.com>
2016-11-07 22:49:00 +0800

    Complete `make_bet` test suite

--- test/lottery.js
+++ test/lottery.js
@@ -1,3 +1,15 @@
+function getTransactionError(func) {
+    return Promise.resolve().then(func)
+        .then(function(txid) {
+            var tx = web3.eth.getTransaction(txid);
+            var txr = web3.eth.getTransactionReceipt(txid);
+            if (txr.gasUsed === tx.gas) throw new Error("all gas used");
+        })
+        .catch(function(err) {
+            return err;
+        });
+}
+
 contract('Lottery', function(accounts) {
 
     it("Should let you buy a ticket", function(done) {
@@ -27,6 +39,62 @@ contract('Lottery', function(accounts) {
                 });
             });
         }).then(done).catch(done);
+    });
+
+    it("Should not let you buy a ticket if you pay more than the price", function() {
+        var getBalance = web3.eth.getBalance;
+        var guess = 42;
+
+        return Lottery.new(1, {from: accounts[3]}).then(function(l) {
+            return l.ticket_price.call().then(function(ticket_price) {
+                return getTransactionError(function() {
+                    return l.make_bet.sendTransaction(guess, {from: accounts[0], value: ticket_price * 2});
+                }).then(function(e) {
+                    assert.isDefined(e, "I got the ticket at twice the price!");
+                });
+            });
+        });
+    });
+
+    it("Should not let you buy a ticket if you guess lower than the lower bound", function() {
+        return Lottery.new(1, {from: accounts[3]}).then(function(l) {
+            return l.ticket_price.call().then(function(ticket_price) {
+                return l.lower_bound.call().then(function(lower_bound) {
+                    return getTransactionError(function() {
+                        return l.make_bet.sendTransaction(lower_bound - 1, {from: accounts[0], value: ticket_price});
+                    }).then(function(e) {
+                        assert.isDefined(e, "I am able to guess a number lower than the lower bound");
+                    });
+                });
+            });
+        });
+    });
+
+    it("Should not let you buy a ticket if you guess higher than the upper bound", function() {
+        return Lottery.new(1, {from: accounts[3]}).then(function(l) {
+            return l.ticket_price.call().then(function(ticket_price) {
+                return l.upper_bound.call().then(function(upper_bound) {
+                    return getTransactionError(function() {
+                        return l.make_bet.sendTransaction(upper_bound + 1, {from: accounts[0], value: ticket_price});
+                    }).then(function(e) {
+                        assert.isDefined(e, "I am able to guess a number higher than the upper bound");
+                    });
+                });
+            });
+        });
+    });
+
+    it("Should not let you buy a ticket if you pay less than the price", function() {
+        var getBalance = web3.eth.getBalance;
+        var guess = 42;
+
+        return Lottery.new(1, {from: accounts[3]}).then(function(l) {
+            return getTransactionError(function() {
+                return l.make_bet.sendTransaction(guess, {from: accounts[0], value: 0});
+            }).then(function(e) {
+                assert.isDefined(e, "I got a ticket for free!");
+            });
+        });
     });
 
     it("Should successfully perform draws", function(done) {
@@ -84,5 +152,4 @@ contract('Lottery', function(accounts) {
             });
         }).then(done).catch(done);
     });
-
 });
